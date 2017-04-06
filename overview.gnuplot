a598abef282cc4692e72d724288d4e6a3749e31f
Jesper Zedlitz <jesper@zedlitz.de>
2017-04-06 19:03:21 +0200

    script to generate diagrams showing the amount of training/test data and the recognition error per book

--- overview.gnuplot
+++ overview.gnuplot
@@ -1,7 +1,31 @@
+set terminal pngcairo size 800,600 enhanced
 set boxwidth 0.5
 set style fill solid 1.0 border -1
-set xtics rotate
-set ylabel 'recognition error'
+set xtics rotate 
+set xtics font ", 9"
 
-plot 'overview.csv' u 4:xtic(1) with boxes notitle, 'overview-error.txt' u 1:2 with lines lc black notitle
-plot 'overview.csv' u 7:xtic(1) with boxes notitle, 'overview-error.txt' u 1:3 with lines lc black notitle
+
+set output 'images/overview-characters.png'
+set title "number of characters used for training/testing"
+set key left top
+set ylabel 'characters'
+plot 'overview.csv' u 3:xtic(1) with boxes title 'training', '' u 6:xtic(1) with boxes title 'testing'
+
+set output 'images/overview-lines.png'
+set title "number of lines used for training/testing"
+set key left top
+set ylabel 'lines'
+plot 'overview.csv' u 2:xtic(1) with boxes title 'training', '' u 5:xtic(1) with boxes title 'testing'
+
+
+set output 'images/overview-training.png'
+set title "character precision for training data"
+set yrange [0:0.1]
+set ylabel 'recognition error'
+plot 'overview.csv' u 4:xtic(1) with boxes notitle, 'overview-error.txt' u ($1-0.5):2 with lines lc black notitle
+
+set output 'images/overview-testing.png'
+set title "character precision for test data"
+set yrange [0:0.1]
+set ylabel 'recognition error'
+plot 'overview.csv' u 7:xtic(1) with boxes notitle, 'overview-error.txt' u ($1-0.5):3 with lines lc black notitle
--- overview.sh
+++ overview.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-# Generate a table with an overview (number of lines, number of characters, recognition error) over the books 
-# used in the training process.
+# Generate a table with an overview (number of lines, number of characters, recognition error) 
+# over the books used in the training process.
 
-ocropus-rpred -q -n -m /home/jzedlitz/sandbox/ocropus-model_fraktur/fraktur.pyrnn.gz -Q6 training/*.bin.png
-ocropus-rpred -q -n -m /home/jzedlitz/sandbox/ocropus-model_fraktur/fraktur.pyrnn.gz -Q6 testing/*.bin.png
+ocropus-rpred -h > /dev/null 2>/dev/null
+if [ $? != 0 ]; then
+	echo "OCRopus not installed correctly. 'ocropus-rpred' could not be executed."
+	exit 1
+fi
+
+echo "Recognizing training data..."
+ocropus-rpred -q -n -m `pwd`/fraktur.pyrnn.gz -Q6 training/*.bin.png
+echo "Recognizing testing data..."
+ocropus-rpred -q -n -m `pwd`/fraktur.pyrnn.gz -Q6 testing/*.bin.png
 
 ls training/*.gt.txt | cut -c 10- | sed 's/_.*//' | uniq  | while read book; do echo -ne "$book\t"; LANG=  wc -m -l training/${book}_*.gt.txt |grep total | sed 's/[^ 0-9].*//' ; done > r1.txt
 ls training/*.gt.txt | cut -c 10- | sed 's/_.*//' | uniq  | while read book; do echo -ne "$book\t"; LANG=  wc -m -l testing/${book}_*.gt.txt |grep total | sed 's/[^ 0-9].*//' ; done > r2.txt
@@ -12,8 +20,17 @@ ls training/*.gt.txt | cut -c 10- | sed 's/_.*//' | uniq  | while read book; do 
 ls training/*.gt.txt | cut -c 10- | sed 's/_.*//' | uniq  | while read book; do echo -ne "$book\t"; ocropus-errs -e training/${book}_*.gt.txt |grep ^0; done > r3.txt
 ls training/*.gt.txt | cut -c 10- | sed 's/_.*//' | uniq  | while read book; do echo -ne "$book\t"; ocropus-errs -e testing/${book}_*.gt.txt |grep ^0; done> r4.txt
 
+NUMBER_OF_BOOKS=`wc -l r1.txt | sed 's/ .*//'`
+ERR_TRAIN=`ocropus-errs -e training/*.gt.txt |grep ^0`
+ERR_TEST=`ocropus-errs -e testing/*.gt.txt |grep ^0`
+
 echo -e "# book\ttraining lines\ttraining characters\ttraining error\ttest lines\ttest characters\ttest error"  > overview.csv
 join r1.txt r3.txt  | join - r2.txt | join - r4.txt | tr ' ' \\t |sed 's/\t\t/\t/' >> overview.csv
 
+echo -e "0\t${ERR_TRAIN}\t${ERR_TEST}" > overview-error.txt
+echo -e "${NUMBER_OF_BOOKS}\t${ERR_TRAIN}\t${ERR_TEST}" >> overview-error.txt
+
+
 echo "Result can be found in 'overview.csv'."
 rm -f r1.txt r2.txt r3.txt r4.txt
+
